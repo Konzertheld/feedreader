c931ed1743e6f8e123b64c6199820a265de6cc05
Konzertheld <christian.gredig@gmail.com>
2013-05-08 12:07:05 +0200

    Simple toggle empty feeds

--- block.readernav.php
+++ block.readernav.php
@@ -1,10 +1,11 @@
 <ul>
+<a href="#" onclick="FeedReader.toggle_empty();">Toggle empty</a>
 <?php foreach($content->navigation as $entry): ?>
-	<li><a href="<?php echo $entry['url']; ?>"><?php echo $entry['title']; ?> (<?php echo $entry['count']; ?>)</a>
+	<li <?php if(!$entry['count']) echo 'class="empty"'; ?>><a href="<?php echo $entry['url']; ?>"><?php echo $entry['title']; ?> (<?php echo $entry['count']; ?>)</a>
 	<?php if(isset($entry['subitems'])): ?>
 	<ul>
 	<?php foreach($entry['subitems'] as $item): ?>
-		<li><a href="<?php echo $item['url']; ?>"><?php echo $item['title']; ?> (<?php echo $item['count']; ?>)</a></li>
+		<li <?php if(!$item['count']) echo 'class="empty"'; ?>><a href="<?php echo $item['url']; ?>"><?php echo $item['title']; ?> (<?php echo $item['count']; ?>)</a></li>
 	<?php endforeach; ?>
 	</ul>
 	<?php endif; ?>
