25f44cea9c3e9982fadf5f00274656930ed1a057
Konzertheld <christian.gredig@gmail.com>
2013-08-15 00:14:39 +0200

    Fix missing dates in feed list

--- admin.feeds_items.php
+++ admin.feeds_items.php
@@ -1,9 +1,10 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-<?php foreach ( $feeds as $feed ):?>
+<?php foreach ( $feeds as $feed ):
+// if($i!=1) {Utils::debug($feed);$i=1;}?>
 	
 	<div class="item clear">
 		<span class="checkbox pct5"><span><input type="checkbox" class="checkbox" name="feed_slugs[]" value="<?php echo $feed['slug']; ?>"></span></span>
-		<span class="time pct20 minor"><span><?php isset($feed['lastcheck']) ? print HabariDateTime::date_create($feed['lastcheck'])->format() : print 'none'; ?></span></span>
+		<span class="time pct20 minor"><span><?php isset($feed['lastcheck']) && !empty($feed['lastcheck']) ? print HabariDateTime::date_create($feed['lastcheck'])->format() : print 'none'; ?></span></span>
 		<span class="pct5 minor"><span>&nbsp;<?php if(isset($feed['brokencount'])) echo $feed['brokencount']; ?></span></span>
 		<span class="pct20 minor"><span><?php if(isset($feed['group'])) echo $feed['group']; else echo "&nbsp;"; ?></span></span>
 		<span class="pct5 minor"><span>&nbsp;<?php if(isset($feed['count'])) echo $feed['count']; ?></span></span>
